644c3b3ed9e7f3457b3a63e39e044fb5cb8e0153
[bruce] <421507690@qq.com>
2020-04-02 17:45:03 +0800

    修改查看商家

--- application/admin/controller/Shop.php
+++ application/admin/controller/Shop.php
@@ -107,7 +107,7 @@ class Shop extends Common
     {
 
         if(Request::instance()->isAjax()){
-            $shop_id    = input('post.id');
+            $shop_id    = input('id');
             $page       = input('page')?input('page'):1;
             $pageSize   = input('limit')?input('limit'):config('pageSize');
             $keyword    = input('key');
