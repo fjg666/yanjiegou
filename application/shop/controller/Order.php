47ef4cf290c49ed10cc3bf60f6d1f1e044418423
[bruce] <421507690@qq.com>
2020-04-08 17:39:03 +0800

    duandian

--- application/shop/controller/Order.php
+++ application/shop/controller/Order.php
@@ -240,7 +240,7 @@ class Order extends Common{
               ->field('og.*,g.title,g.headimg')
               ->where(['og.id'=>$info['og_id']])
               ->select();
-        var_dump($goods);die;
+        var_dump($info);die;
         $counts=0; 
         $info['imgs']=explode(',',$info['imgs']);
         $info['counts']=$goods[0]['num']*$goods[0]['price'];
