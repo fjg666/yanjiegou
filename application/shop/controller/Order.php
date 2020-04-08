6ecc569f91b6bd38396fc96ae5baa7b26843d027
[bruce] <421507690@qq.com>
2020-04-08 15:07:15 +0800

    duandian

--- application/shop/controller/Order.php
+++ application/shop/controller/Order.php
@@ -127,7 +127,7 @@ class Order extends Common{
             ->where('infouid',$shopid)
             ->where("uid",'user'.$info['user_id'])
             ->find();
-        var_dump($sel);die;
+       echo Db::name('chat')->getLastSql();die;
         $uid = str_replace('user','',$sel['uid']);
         $users = Db::name('users')->field('username,mobile,avatar')->where('id',$uid)->find();
         $sel['mobile'] = $users['mobile'];
